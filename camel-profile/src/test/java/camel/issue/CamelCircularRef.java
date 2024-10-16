e3529cce3a9b6094e3bcf3fd26fa825f016d8b0a
Carsten Madsen <com@navicon.dk>
2024-10-16 09:26:02 +0200

    Simplified

--- camel-profile/src/test/java/camel/issue/CamelCircularRef.java
+++ camel-profile/src/test/java/camel/issue/CamelCircularRef.java
@@ -1,33 +1,25 @@
 package camel.issue;
 
-import static org.assertj.core.api.Assertions.*;
-
-import org.apache.camel.CamelContext;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+// @ImportResource("classpath:XmlConfigCircular.xml")
 // @ExtendWith(SpringExtension.class)
 // @ContextConfiguration(initializers =
 // CamelCircularRef.XmlConfigInitializer.class)
-@ActiveProfiles("circular")
+// @ActiveProfiles("circular")
 class CamelCircularRef {
 
-	@Autowired
-	private CamelContext camelContext;
+	// @Autowired
+	// private CamelContext camelContext;
 
 	@Test
 	void testCamelConfig() {
 		try {
-			assertThat(camelContext).isNotNull();
+			// assertThat(camelContext).isNotNull();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -38,21 +30,21 @@ class CamelCircularRef {
 	static class TestConfig {
 	}
 
-	public static class XmlConfigInitializer implements
-			ApplicationContextInitializer<GenericApplicationContext> {
-		@Override
-		public void initialize(GenericApplicationContext context) {
-			String[] activeProfiles = context.getEnvironment()
-					.getActiveProfiles();
-			for (String profile : activeProfiles) {
-				if ("circular".equals(profile)) {
-					XmlBeanDefinitionReader xmlReader = new XmlBeanDefinitionReader(
-							context);
-					xmlReader.loadBeanDefinitions(
-							new ClassPathResource("XmlConfigCircular.xml"));
-				}
-			}
-		}
-	}
+	// public static class XmlConfigInitializer implements
+	// ApplicationContextInitializer<GenericApplicationContext> {
+	// @Override
+	// public void initialize(GenericApplicationContext context) {
+	// String[] activeProfiles = context.getEnvironment()
+	// .getActiveProfiles();
+	// for (String profile : activeProfiles) {
+	// if ("circular".equals(profile)) {
+	// XmlBeanDefinitionReader xmlReader = new XmlBeanDefinitionReader(
+	// context);
+	// xmlReader.loadBeanDefinitions(
+	// new ClassPathResource("XmlConfigCircular.xml"));
+	// }
+	// }
+	// }
+	// }
 
 }
