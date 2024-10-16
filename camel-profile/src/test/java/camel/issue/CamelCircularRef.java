26744c21008af9b588df882f8d44467d5d6e2593
Carsten Madsen <com@navicon.dk>
2024-10-16 08:20:02 +0200

    Change to failing config setup

--- camel-profile/src/test/java/camel/issue/CamelCircularRef.java
+++ camel-profile/src/test/java/camel/issue/CamelCircularRef.java
@@ -4,19 +4,20 @@ import static org.assertj.core.api.Assertions.*;
 
 import org.apache.camel.CamelContext;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration(initializers = CamelCircularRef.XmlConfigInitializer.class)
+@SpringBootTest
+// @ExtendWith(SpringExtension.class)
+// @ContextConfiguration(initializers =
+// CamelCircularRef.XmlConfigInitializer.class)
 @ActiveProfiles("circular")
 class CamelCircularRef {
 
@@ -33,7 +34,7 @@ class CamelCircularRef {
 	}
 
 	@Configuration
-	// @ImportResource("classpath:XmlConfigCircular.xml")
+	@ImportResource("classpath:XmlConfigCircular.xml")
 	static class TestConfig {
 	}
 
